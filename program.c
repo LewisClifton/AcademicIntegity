e4d536e4969e192040bef4988e9b1ab6c522b30f
LewisClifton <lewisclifton2003@gmail.com>
2022-02-01 10:52:53 +0000

    testing merge issues

--- program.c
+++ program.c
@@ -3,5 +3,7 @@
 int main() {
     printf("Oh no hope this doesn't cause a merge issue lol");
     printf("Hello there");
+    printf("wicked cheeseburger");
+    printf("jazz music");
     return 0;
 }
